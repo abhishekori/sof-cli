df5d8fddce935c939a878b81a123f0ffd14eda72
abhishekori <abhikori1994@gmail.com>
2018-07-21 00:00:40 +0530

    basic app doen and installed the cli

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const { prompt } = require('inquirer');
+
+console.log("Hello world")
+
+// program
+//   .command('addContact') // No need of specifying arguments here
+//   .alias('a')
+//   .description('Add a contact')
+//   .action(() => {
+//     prompt(questions).then(answers =>
+//       console.log("hello");
+//   });
